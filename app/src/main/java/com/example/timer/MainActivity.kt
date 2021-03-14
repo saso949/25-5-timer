c0426d61eb1877c60ca71b3091d24c0016eea1a6
saso949 <arukimakurou2@gmail.com>
2021-03-14 18:00:03 +0900

    5秒を数える機能を追加

--- app/src/main/java/com/example/timer/MainActivity.kt
+++ app/src/main/java/com/example/timer/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.timer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var cnt = 0
+    val hnd0 = Handler()
+
+    val rnb0=object : Runnable{
+        override fun run() {
+            cnt++
+            countView.text=cnt.toString()
+            if(cnt<50){
+                hnd0.postDelayed(this,100)
+            }
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startButton.setOnClickListener(){
+            hnd0.post(rnb0)
+        }
+
+
+
     }
 }
