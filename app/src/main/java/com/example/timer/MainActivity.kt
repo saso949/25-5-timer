a0e0a50dcca6ea35146e9c4fea1ae14055b3f418
saso949 <arukimakurou2@gmail.com>
2021-03-15 15:55:43 +0900

    25分が終わった時に流れる音楽を再設定

--- app/src/main/java/com/example/timer/MainActivity.kt
+++ app/src/main/java/com/example/timer/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.timer
 
+import android.media.AudioAttributes
+import android.media.SoundPool
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
@@ -7,6 +9,11 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var soundPool: SoundPool
+    private var soundOne = 0
+
+
     var cnt = 0
     var viewCount: Int = 0
     var a = 0
@@ -40,15 +47,37 @@ class MainActivity : AppCompatActivity() {
 
                 if (viewCount == 1500){
                     Log.d("お知らせ","25分経ちました！")
+                    soundPool.play(soundOne, 1.0f, 1.0f, 0, 0, 1.0f)
+                    Handler().post(rnb0)
                 }
             }
         }
     }
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val audioAttributes = AudioAttributes.Builder()
+            // USAGE_MEDIA
+            // USAGE_GAME
+            .setUsage(AudioAttributes.USAGE_GAME)
+            // CONTENT_TYPE_MUSIC
+            // CONTENT_TYPE_SPEECH, etc.
+            .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+            .build()
+
+        soundPool = SoundPool.Builder()
+            .setAudioAttributes(audioAttributes)
+            // ストリーム数に応じて
+            .setMaxStreams(2)
+            .build()
+
+        // one.wav をロードしておく
+        soundOne = soundPool.load(this, R.raw.one, 1)
+
         startButton.setOnClickListener() {
             Handler().post(rnb0)
         }
