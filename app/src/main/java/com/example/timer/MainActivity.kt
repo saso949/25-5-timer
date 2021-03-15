7c56fa6d50f15241ae03fe01f3ad3222b7e8c57a
saso949 <arukimakurou2@gmail.com>
2021-03-15 10:17:16 +0900

    25分までのカウントを表示する機能を追加

--- app/src/main/java/com/example/timer/MainActivity.kt
+++ app/src/main/java/com/example/timer/MainActivity.kt
@@ -2,12 +2,13 @@ package com.example.timer
 
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     var cnt = 0
-    var viewCount :Int = 0
+    var viewCount: Int = 0
     var a = 0
     var b = 0
     val hnd0 = Handler()
@@ -16,19 +17,30 @@ class MainActivity : AppCompatActivity() {
         override fun run() {
             cnt += 1
             viewCount = cnt / 10
+            a = viewCount / 60
+            b = viewCount % 60
+
             hnd0.postDelayed(this, 100)
 
             if (viewCount < 10) {
                 countView.text = "00:0" + viewCount
-            }else if (viewCount < 60){
+            } else if (viewCount < 60) {
                 countView.text = "00:" + viewCount
-            }else if (viewCount < 600){
-                a = viewCount / 60
-                b = viewCount % 60
-                if (b < 10){
-                    countView.text = "0" + a + ":0" + b
-                }else{
-                    countView.text = "0" + a + ":" + b
+            } else if (viewCount < 600) {
+                if (b < 10) {
+                    countView.text = "0" + a.toString() + ":0" + b
+                } else {
+                    countView.text = "0" + a.toString() + ":" + b
+                }
+            }else if (viewCount < 1501){
+                if (b < 10) {
+                    countView.text = a.toString() + ":0" + b
+                } else {
+                    countView.text = a.toString() + ":" + b
+                }
+
+                if (viewCount == 1500){
+                    Log.d("お知らせ","25分経ちました！")
                 }
             }
         }
@@ -41,7 +53,6 @@ class MainActivity : AppCompatActivity() {
         startButton.setOnClickListener() {
             hnd0.post(rnb0)
         }
-
 
 
     }
