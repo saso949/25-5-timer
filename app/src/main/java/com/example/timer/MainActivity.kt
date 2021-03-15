f5f395c231a73da6b4c376e0f77d4d262bb707c1
saso949 <arukimakurou2@gmail.com>
2021-03-15 19:35:50 +0900

    25分と5分を交互に再生、また25分タイマーを4回再生したら30分のタイマーが再生される機能を追加

--- app/src/main/java/com/example/timer/MainActivity.kt
+++ app/src/main/java/com/example/timer/MainActivity.kt
@@ -13,86 +13,109 @@ class MainActivity : AppCompatActivity() {
     private lateinit var soundPool: SoundPool
     private var soundOne = 0
 
-
+    var tt = 0
     var cnt = 0
     var viewCount: Int = 0
     var a = 0
     var b = 0
 
-   val rnb0 = object : Runnable {
+    var rnb0 = object : Runnable {
         override fun run() {
+
             cnt += 1
             viewCount = cnt / 10
             a = viewCount / 60
             b = viewCount % 60
 
-            Handler().postDelayed(this, 100)
+            //30分タイマー
+            if (tt == 7 || tt == 15 || tt == 23 || tt == 31 || tt == 39 || tt == 47) {
+                if (viewCount < 10) {
 
-            if (viewCount < 10) {
-                countView.text = "00:0" + viewCount
-            } else if (viewCount < 60) {
-                countView.text = "00:" + viewCount
-            } else if (viewCount < 600) {
-                if (b < 10) {
-                    countView.text = "0" + a.toString() + ":0" + b
-                } else {
-                    countView.text = "0" + a.toString() + ":" + b
-                }
-            }else if (viewCount < 1501){
-                if (b < 10) {
-                    countView.text = a.toString() + ":0" + b
-                } else {
-                    countView.text = a.toString() + ":" + b
-                }
+                    countView.text = "00:0" + viewCount
+                } else if (viewCount < 60) {
+                    countView.text = "00:" + viewCount
+                } else if (viewCount < 600) {
+                    if (b < 10) {
+                        countView.text = "0" + a.toString() + ":0" + b
+                    } else {
+                        countView.text = "0" + a.toString() + ":" + b
+                    }
+                } else if (viewCount < 1801) {
+                    if (b < 10) {
+                        countView.text = a.toString() + ":0" + b
+                    } else {
+                        countView.text = a.toString() + ":" + b
+                    }
 
-                if (viewCount == 1500){
-                    Log.d("お知らせ","25分経ちました！")
-                    cnt = 0
-                    viewCount = 0
-                    a = 0
-                    b = 0
-                    soundPool.play(soundOne, 1.0f, 1.0f, 0, 0, 1.0f)
-                    Handler().post(rnb1)
+                    if (viewCount == 1800) {
+                        Log.d("お知らせ", "30分経ちました！")
+                        tt += 1
+                        cnt = 0
+                        viewCount = 0
+                        a = 0
+                        b = 0
+                        soundPool.play(soundOne, 1.0f, 1.0f, 0, 0, 1.0f)
+                    }
                 }
+            } else if (tt % 2 == 0) {
+
+                if (viewCount < 10) {
+                    countView.text = "00:0" + viewCount
+                } else if (viewCount < 60) {
+                    countView.text = "00:" + viewCount
+                } else if (viewCount < 600) {
+                    if (b < 10) {
+                        countView.text = "0" + a.toString() + ":0" + b
+                    } else {
+                        countView.text = "0" + a.toString() + ":" + b
+                    }
+                } else if (viewCount < 1501) {
+                    if (b < 10) {
+                        countView.text = a.toString() + ":0" + b
+                    } else {
+                        countView.text = a.toString() + ":" + b
+                    }
+
+                    if (viewCount == 1500) {
+                        Log.d("お知らせ", "25分経ちました！")
+                        tt += 1
+                        cnt = 0
+                        viewCount = 0
+                        a = 0
+                        b = 0
+                        countView.text = "00:00"
+                        soundPool.play(soundOne, 1.0f, 1.0f, 0, 0, 1.0f)
+                    }
+                }
+            } else if (tt % 2 == 1) {
+
+                if (viewCount < 10) {
+                    countView.text = "00:0" + viewCount
+                } else if (viewCount < 60) {
+                    countView.text = "00:" + viewCount
+                } else if (viewCount < 600) {
+                    if (b < 10) {
+                        countView.text = "0" + a.toString() + ":0" + b
+                    } else {
+                        countView.text = "0" + a.toString() + ":" + b
+                    }
+                    if (viewCount == 300) {
+                        Log.d("お知らせ", "5分がたちました！")
+                        tt += 1
+                        cnt = 0
+                        viewCount = 0
+                        a = 0
+                        b = 0
+                        soundPool.play(soundOne, 1.0f, 1.0f, 0, 0, 1.0f)
+                    }
+                }
+            }
+
+            if (cnt < 18006) {
+                Handler().postDelayed(this, 100)
             }
         }
     }
-
-    val rnb1 = object : Runnable {
-        override fun run() {
-            cnt += 1
-            viewCount = cnt / 10
-            a = viewCount / 60
-            b = viewCount % 60
-
-            Handler().postDelayed(this, 100)
-
-            if (viewCount < 10) {
-                countView.text = "00:0" + viewCount
-            } else if (viewCount < 60) {
-                countView.text = "00:" + viewCount
-            } else if (viewCount < 600) {
-                if (b < 10) {
-                    countView.text = "0" + a.toString() + ":0" + b
-                } else {
-                    countView.text = "0" + a.toString() + ":" + b
-                }
-            }else if (viewCount < 1501){
-                if (b < 10) {
-                    countView.text = a.toString() + ":0" + b
-                } else {
-                    countView.text = a.toString() + ":" + b
-                }
-
-                if (viewCount == 300){
-                    Log.d("お知らせ","5分がたちました！")
-                    soundPool.play(soundOne, 1.0f, 1.0f, 0, 0, 1.0f)
-                    Handler().post(rnb0)
-                }
-            }
-        }
-    }
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
