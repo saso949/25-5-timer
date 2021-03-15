92b139a54c2bcb5bea9dbeeb99fa8e60c6d1f953
saso949 <arukimakurou2@gmail.com>
2021-03-15 09:27:29 +0900

    9秒までのカウントを表示する機能を追加

--- app/src/main/java/com/example/timer/MainActivity.kt
+++ app/src/main/java/com/example/timer/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.example.timer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     var cnt = 0
+    var viewCount :Int = 0
     val hnd0 = Handler()
 
-    val rnb0=object : Runnable{
+    val rnb0 = object : Runnable {
         override fun run() {
-            cnt++
-            countView.text=cnt.toString()
-            if(cnt<50){
-                hnd0.postDelayed(this,100)
+            cnt += 1
+            viewCount = cnt / 10
+            hnd0.postDelayed(this, 100)
+
+            if (viewCount < 10) {
+                countView.text = "00:0" + viewCount
             }
         }
     }
@@ -23,8 +26,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        startButton.setOnClickListener(){
+        startButton.setOnClickListener() {
             hnd0.post(rnb0)
         }
 
