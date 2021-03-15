ed0f048d20d1fa0324cf37070e218d451746d742
saso949 <arukimakurou2@gmail.com>
2021-03-15 15:57:06 +0900

    25分の後に5分タイマーを再生する機能を追加

--- app/src/main/java/com/example/timer/MainActivity.kt
+++ app/src/main/java/com/example/timer/MainActivity.kt
@@ -48,6 +48,41 @@ class MainActivity : AppCompatActivity() {
                 if (viewCount == 1500){
                     Log.d("お知らせ","25分経ちました！")
                     soundPool.play(soundOne, 1.0f, 1.0f, 0, 0, 1.0f)
+                    Handler().post(rnb1)
+                }
+            }
+        }
+    }
+
+    val rnb1 = object : Runnable {
+        override fun run() {
+            cnt += 1
+            viewCount = cnt / 10
+            a = viewCount / 60
+            b = viewCount % 60
+
+            Handler().postDelayed(this, 100)
+
+            if (viewCount < 10) {
+                countView.text = "00:0" + viewCount
+            } else if (viewCount < 60) {
+                countView.text = "00:" + viewCount
+            } else if (viewCount < 600) {
+                if (b < 10) {
+                    countView.text = "0" + a.toString() + ":0" + b
+                } else {
+                    countView.text = "0" + a.toString() + ":" + b
+                }
+            }else if (viewCount < 1501){
+                if (b < 10) {
+                    countView.text = a.toString() + ":0" + b
+                } else {
+                    countView.text = a.toString() + ":" + b
+                }
+
+                if (viewCount == 300){
+                    Log.d("お知らせ","5分がたちました！")
+                    soundPool.play(soundOne, 1.0f, 1.0f, 0, 0, 1.0f)
                     Handler().post(rnb0)
                 }
             }
